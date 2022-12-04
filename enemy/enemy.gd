ea1bca76eeb95e7867a55e222cd7ecf7fc3eee75
Dmitrii Borisov <rundoom1337@gmail.com>
2022-12-05 01:32:56 +0700

    AI implemented

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -3,4 +3,86 @@ class_name Enemy
 
 
 @export var hp: int
-@export var SPEED : int
+@export var SPEED: int
+var BASE_SAFE_MARGIN := safe_margin
+@onready var hero = get_tree().get_first_node_in_group("hero")
+@onready var hero_detector:= $HeroDetector as RayCast2D
+@onready var detection_timer = $DetectionTimer as Timer
+@onready var navigator = $Navigator as NavigationAgent2D
+
+enum State {IDLE, STALK, SEEK, PANIC, RETREAT, ATTACK}
+
+
+var state_map = {
+	State.IDLE: idle,
+	State.STALK: stalk,
+	State.SEEK: seek,
+	State.PANIC: panic,
+	State.RETREAT: retreat,
+	State.ATTACK: attack
+}
+
+var state: State = State.STALK
+
+
+func _ready() -> void:
+	navigator.max_speed = SPEED
+	navigator.radius = $Collider.shape.radius + BASE_SAFE_MARGIN / 2
+
+
+func _physics_process(delta: float) -> void:
+	$State.text = State.keys()[state]
+	state_map[state].call()
+
+
+func idle():
+	set_process(false)
+	
+	
+func stalk():
+	velocity = global_position.direction_to(hero.global_position) * SPEED
+		
+	move_and_slide()
+	if get_real_velocity().length() < velocity.length() / 5:
+		velocity = velocity.rotated(PI / 2)
+		move_and_slide()
+	
+	
+func seek():
+	$Path.points = Array(navigator.get_nav_path()).map(func(it): return it - position)
+	velocity = global_position.direction_to(navigator.get_next_location()) * SPEED
+	navigator.set_velocity(velocity)
+	
+	
+func panic():
+	pass
+	
+	
+func attack():
+	pass
+	
+	
+func retreat():
+	pass
+	
+
+func _on_detection_timer_timeout() -> void:
+	hero_detector.enabled = true
+	hero_detector.global_rotation = hero_detector.global_position.angle_to_point(hero.global_position)
+	hero_detector.force_raycast_update()
+	var collider = hero_detector.get_collider()
+	hero_detector.enabled = false
+	
+	if collider.owner == hero:
+		if state == State.SEEK:
+			state = State.STALK
+		$Path.points = []
+	else:
+		state = State.SEEK
+		navigator.target_location = hero.global_position
+
+
+func _on_navigator_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
+
+	move_and_slide()
--- enemy/melee/melee_enemy.gd
+++ enemy/melee/melee_enemy.gd
@@ -1,25 +1,24 @@
 extends Enemy
 
 
-var BASE_SAFE_MARGIN := safe_margin
 var safe_margin_tween : Tween
 
 
-func _physics_process(delta: float) -> void:
-	var hero := get_tree().get_first_node_in_group("hero") as CharacterBody2D
-	var target := global_position.direction_to(hero.global_position).angle()
-	velocity = Vector2.RIGHT.rotated(target) * SPEED
-		
-	move_and_slide()
-	
-
 func _on_charge_distance_area_entered(area: Area2D) -> void:
+	state = State.ATTACK
 	if safe_margin_tween: safe_margin_tween.kill()
 	safe_margin_tween = create_tween()
 	safe_margin_tween.tween_property(self, "safe_margin", 0, 0.5)
 
 
 func _on_charge_distance_area_exited(area: Area2D) -> void:
+	state = State.STALK
 	if safe_margin_tween: safe_margin_tween.kill()
 	safe_margin_tween = create_tween()
 	safe_margin_tween.tween_property(self, "safe_margin", BASE_SAFE_MARGIN, 0.5)
+
+
+func attack():
+	velocity = global_position.direction_to(hero.global_position) * SPEED
+		
+	move_and_slide()
